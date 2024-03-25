db086de06f01990a9c58a29ae953f640af6f683f
taewoocode <qkrxodn6035@gmail.com>
2024-03-25 11:14:09 +0900

    보안 의존성 추가

--- src/main/java/me/denden/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/denden/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,4 +1,4 @@
-package me.shinsunyoung.springbootdeveloper;
+package me.shinsunyg.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
