336c615796314929dc6ba0f38319b6920cc93468
taewoocode <qkrxodn6035@gmail.com>
2024-03-27 20:56:30 +0900

    로그아웃 컨트롤러 구현

--- src/main/java/me/denden/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/denden/springbootdeveloper/controller/UserApiController.java
@@ -1,10 +1,15 @@
 package me.denden.springbootdeveloper.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.denden.springbootdeveloper.dto.AddUserRequest;
 import me.denden.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -18,5 +23,13 @@ public class UserApiController {
         userService.save( request ); //userService객체
         return "redirect:/login"; //회원 가입 완료이후 login page로 이동한다.
 
+    }
+
+    //로그아웃 기능 구현
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout( request,response,
+                SecurityContextHolder.getContext().getAuthentication() );
+        return "redirect/login";
     }
 }
